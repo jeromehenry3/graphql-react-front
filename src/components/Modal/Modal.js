33542619ccb1be19f84f0e6b34604b8a8eee1307
jerome <jerome.henry3@gmail.com>
2020-01-19 14:40:30 +0100

    various fixes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { Button } from 'semantic-ui-react';
 import './Modal.css';
 
@@ -11,10 +12,17 @@ const Modal = props => (
             {props.children}
         </section>
         <section className="modal__actions">
-            {props.canCancel && <Button className="button">Annuler</Button>}
-            {props.canConfirm && <Button className="button">Confirmer</Button>}
+            {props.canCancel && <Button className="button" onClick={props.canCancel}>Annuler</Button>}
+            {props.canConfirm && <Button className="button" onClick={props.canConfirm}>Confirmer</Button>}
         </section>
     </div>
 )
+
+Modal.propTypes = {
+    title: PropTypes.string.isRequired,
+    canCancel: PropTypes.func,
+    canConfirm: PropTypes.func,
+    children: PropTypes.any,
+}
 
 export default Modal;
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,4 +1,4 @@
-import React, { Component, propTypes } from 'react';
+import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
@@ -6,23 +6,32 @@ import Backdrop from '../components/Backdrop/Backdrop';
 import './Events.css';
 
 class EventsPage extends Component {
+    state = {
+        creating: false,
+    }
 
+    handleCreateEventButton = () => {
+        this.setState({creating: true});
+    }
+
+    handleCancelInModal = () => {
+        this.setState({creating: false});
+    }
 
     render() {
         return (
             <>
-                <Backdrop></Backdrop>
-                <Modal title="créer un évènement" canCancel canConfirm>{<p>Modal Content</p>}</Modal>
+                {this.state.creating && 
+                <>
+                    <Backdrop></Backdrop>
+                    <Modal title="créer un évènement" canCancel={this.handleCancelInModal} canConfirm={this.handleCancelInModal}>{<p>Modal Content</p>}</Modal>
+                </>}
                 <div className="create-event_button">
-                    <Button>Créer un évènement</Button>
+                    <Button onClick={this.handleCreateEventButton}>Créer un évènement</Button>
                 </div>
             </>
         );
     }
-}
-
-EventsPage.propTypes = {
-
 }
 
 export default EventsPage;
